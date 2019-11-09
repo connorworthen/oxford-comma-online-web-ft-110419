5a3d725226617ee82b7e61280b05d2a9d9052649
connor worthen <connorw220@protonmail.com>
2019-11-09 05:56:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
   array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
   ["kiwi"].join
+    puts result
   ["kiwi", "durian"].join(" and ")
+    puts result
 end
